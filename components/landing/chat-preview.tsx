ddabf11e2436c12b81e69f55408ff6247deadfa3
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2026-05-05 21:44:33 +0700

    Update chat-preview.tsx

--- components/landing/chat-preview.tsx
+++ components/landing/chat-preview.tsx
@@ -39,7 +39,7 @@ export default function ChatPreview() {
       {/* Background */}
       <div className="absolute inset-0">
         <div className="absolute top-0 left-1/4 w-96 h-96 bg-blue-500/10 rounded-full blur-3xl" />
-        <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-purple-500/10 rounded-full blur-3xl" />
+        <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-blue-500/10 rounded-full blur-3xl" />
       </div>
 
       <div className="container mx-auto px-4 relative z-10">
