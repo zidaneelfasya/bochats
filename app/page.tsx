9b7828021a7dd165289ec53cbbfb25382712dc2f
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2026-05-12 21:37:45 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
       <ChatPreview />
       {/* <VideoTutorial /> */}
       {/* <ProductDemoSection /> */}
+      
       <ProductJourneySection />
       <PricingModern />
       <CTAModern />
